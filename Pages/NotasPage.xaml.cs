472b2f1e4f35b088d73821d6785c4554b621b9a1
kenyaa1987 <bangdxidar@gmail.com>
2025-06-20 01:41:29 -0500

    Agregue estructura de los achivos: Notas y materias.

--- Pages/NotasPage.xaml.cs
+++ Pages/NotasPage.xaml.cs
@@ -2,8 +2,17 @@ namespace MiAgendaEscolar.Pages;
 
 public partial class NotasPage : ContentPage
 {
+    private string ultimaNota = "";
+
     public NotasPage()
     {
         InitializeComponent();
+    }
+
+    private void GuardarNota_Clicked(object sender, EventArgs e)
+    {
+        ultimaNota = NotaEditor.Text;
+        NotaMostrada.Text = ultimaNota;
+        DisplayAlert("Nota guardada", "Tu nota ha sido guardada temporalmente.", "OK");
     }
 }
